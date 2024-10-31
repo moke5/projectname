fd530f6cfa9f2feb529422188c60dd6622489f68
moke5 <143516628+moke5@users.noreply.github.com>
2024-10-31 09:12:43 +0800

    Update Example4_22.java

--- code/src/Example4_22.java
+++ code/src/Example4_22.java
@@ -4,7 +4,7 @@ class Student {
 	String name;
 
 	void speak() {
-		System.out.println("Ñ§ºÅ£º" + number + "\nĞÕÃû£º" + name);
+		System.out.println("å­¦å·ï¼š" + number + "\nå§“åï¼š" + name);
 	}
 }
 
@@ -14,19 +14,13 @@ public class Example4_22 {
 		// TODO Auto-generated method stub
 		Student stu1 = new Student();
 		stu1.speak();
-		stu1.name = "Ñ©Ó£´óÊå";
+		stu1.name = "é›ªæ¨±å¤§å”";
 		stu1.number = 2023115208;
 		stu1.speak();
 
 		Student stu[];
-		stu = new Student[10];
-
-//		stu[3] = new Student();
-//		stu[3].name = "Ñ©Ó£´óÊåÈıºÅ";
-//		stu[3].number = 2023115208;
-//		stu[3].speak();
-//		 
-
+		stu = new Student[10];	 
+		
 		for (int i = 0; i < stu.length; i++)
 			stu[i] = new Student();
 
